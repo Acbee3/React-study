eb2281a85f36e2115ad797d369b339ee9fdc39ff
Acbee3 <37231852+Acbee3@users.noreply.github.com>
2019-11-01 01:23:48 +0800

    122

--- 01/src/index.js
+++ 01/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// 这里新增了东西
+// 这里新增了东西, 但我又取消了
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
